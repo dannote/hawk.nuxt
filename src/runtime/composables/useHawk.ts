d7e61139fc2d62edf5ca7ede7e28fd9e2a957bfe
Peter Savchenko <specc.dev@gmail.com>
2024-09-26 18:00:37 +0300

    add docs

--- src/runtime/composables/useHawk.ts
+++ src/runtime/composables/useHawk.ts
@@ -3,11 +3,22 @@ import {
   useNuxtApp,
 } from '#imports'
 
+/**
+ * Composable for working with errors tracker
+ */
 export default function useHawk() {
   const { $hawk } = useNuxtApp()
 
+  /**
+   * Current hawk instance
+   */
   const hawkInstance = $hawk as Catcher
 
+  /**
+   * Method for manual sending errors and logs
+   *
+   * @param message - text or error to track
+   */
   function send(message: string | Error) {
     hawkInstance.send(message)
   }
