6504fc279b080358a0fa5f153f1e4e467f7d9cbf
Peter Savchenko <specc.dev@gmail.com>
2024-09-29 17:46:40 +0300

    Update nuxt.config.ts

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -1,13 +1,4 @@
 export default defineNuxtConfig({
-  // sourcemap: {
-  //   client: 'hidden',
-  //   server: 'hidden',
-  // },
-  // vite: {
-  //   build: {
-  //     sourcemap: 'inline',
-  //   },
-  // },
   modules: ['../src/module'],
   hawk: {
     token: process.env.HAWK_TOKEN,
